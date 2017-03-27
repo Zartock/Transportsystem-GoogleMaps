e0b1b89d4e048395d66e5e46be58b33f413d70a4
Viktor Karlsson <virrevickan95@hotmail.com>
2017-03-27 15:45:33 +0200

    första commiten

---
+++ Transportsystem-GoogleMaps/Transportsystem-GoogleMaps/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Transportsystem_GoogleMaps
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Transportsystem-GoogleMaps/Transportsystem-GoogleMaps/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Transportsystem_GoogleMaps.MvcApplication" Language="C#" %>
