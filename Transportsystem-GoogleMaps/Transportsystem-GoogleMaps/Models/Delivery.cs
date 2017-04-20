50cab151b7701e2a0addd8a89f2438ab037cd6ad
Zumi123 <einar_larsson@hotmail.com>
2017-04-20 13:55:23 +0200

    cleaned clustering code

--- Transportsystem-GoogleMaps/Transportsystem-GoogleMaps/Models/Delivery.cs
+++ Transportsystem-GoogleMaps/Transportsystem-GoogleMaps/Models/Delivery.cs
@@ -80,7 +80,6 @@ namespace Transportsystem_GoogleMaps.Models
                         packageIndex = i;
                         avNumbers[i] = -1;
                     }
-                    
                 }
                 i++;
             }
@@ -108,70 +107,10 @@ namespace Transportsystem_GoogleMaps.Models
                 p.Longitude = avgLng + 0.05 * Math.Sin(angle);
                 clusterCentroids.AddLast(new PackageCluster(p));
             }
-            
-            
-            /*Random rnd = new Random();
-            List<int> avNumbers = new List<int>();
-            for (int n = 0; n < Packages.Count; n++)
-            {
-                avNumbers.Add(n);
-            }
-
-
-            bool notAssigned;
-            for (int i = 0; i < numOfDrivers; i++)
-            {
-                notAssigned = true;
-                while (notAssigned)
-                {
-                    int packageIndex = rnd.Next(Packages.Count);
-
-                    if (avNumbers.Contains(packageIndex))
-                    {
-                        clusterCentroids.AddLast(new PackageCluster(Packages.ElementAt(packageIndex)));
-                        notAssigned = false;
-                        avNumbers[packageIndex] = -1;
-                    }
-                }
-            }*/
-
-
-
         }
-
-        public void InitializeClusters(ref LinkedList<PackageCluster> clusterCentroids, List<double> latList, List<double> lngList)
-        {        
-            for (int i = 0; i < numOfDrivers; i++)
-            {
-                //notAssigned = true;
-                //while (notAssigned)
-                //{
-                //    int packageIndex = rnd.Next(Packages.Count);
-
-                //    if (avNumbers.Contains(packageIndex))
-                //    {
-                //        clusterCentroids.AddLast(new PackageCluster(Packages.ElementAt(packageIndex)));
-                //        notAssigned = false;
-                //        avNumbers[packageIndex] = -1;
-                //    }
-                //}
-
-                Package p = new Package();
-                p.Latitude = latList[i];
-                p.Longitude = latList[i];
-                clusterCentroids.AddLast(new PackageCluster(p));
-            }
-        }
-
-
 
         public void AssignPackagesToNearestCluster(ref LinkedList<PackageCluster> clusterCentroids)
         {
-            //foreach (var cluster in clusterCentroids)
-            //{
-            //    cluster.AssignedPackages.Clear();
-            //}
-
             foreach (var package in Packages)
             {
                 double closestDistance = Double.PositiveInfinity;
@@ -220,47 +159,7 @@ namespace Transportsystem_GoogleMaps.Models
 
         public LinkedList<PackageCluster> clustering()
         {
-    
-            LinkedList<LinkedList<PackageCluster>> clusterCentroidsOptions = new LinkedList<LinkedList<PackageCluster>>();
             bool changes = true;
-            List<double> latList = new List<double>();
-            List<double> lngList = new List<double>();
-            int iterations = 1;
-            for (int i = 0; i < numOfDrivers; i++)
-            {
-                latList.Add(0);
-                lngList.Add(0);
-            }
-
-            for (int k = 0; k < iterations; k++)
-            {
-                changes = true;
-                LinkedList<PackageCluster> clusterCentroids = new LinkedList<PackageCluster>();
-                InitializeClusters(ref clusterCentroids);
-
-                //first init
-                AssignPackagesToNearestCluster(ref clusterCentroids);
-                AlterCentroidPosition(ref clusterCentroids, ref changes);
-
-                //repeat until no changes
-                while (changes)
-                {
-                    AssignPackagesToNearestCluster(ref clusterCentroids);
-                    AlterCentroidPosition(ref clusterCentroids, ref changes);
-                }
-                for (int i = 0; i < numOfDrivers; i++)
-                {
-                    latList[i] += clusterCentroids.ElementAt(i).CentroidPackage.Latitude;
-                    lngList[i] += clusterCentroids.ElementAt(i).CentroidPackage.Longitude;
-                }
-            }
-
-            for (int i = 0; i < numOfDrivers; i++)
-            {
-                latList[i] /= iterations;
-                lngList[i] /= iterations;
-            }
-
             LinkedList<PackageCluster> clusterCent = new LinkedList<PackageCluster>();
             InitializeClusters(ref clusterCent);
 
@@ -270,18 +169,12 @@ namespace Transportsystem_GoogleMaps.Models
             //repeat until no changes
             while (changes)
             {
+                foreach (var cluster in clusterCent)
+                {
+                    cluster.AssignedPackages.Clear();
+                }
                 AssignPackagesToNearestCluster(ref clusterCent);
                 AlterCentroidPosition(ref clusterCent, ref changes);
-            }
-
-            foreach (var cluster in clusterCent)
-            {
-                if (cluster.AssignedPackages.Count < 3)
-                {
-                    clusterCent = clustering();
-                    break;
-                }
-                    
             }
             return clusterCent;
         }
