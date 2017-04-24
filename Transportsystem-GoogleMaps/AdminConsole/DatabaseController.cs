68ea21640e485f933a666189da3c3a8857f756fb
Viktor Karlsson <virrevickan95@hotmail.com>
2017-04-24 11:08:43 +0200

    Added console application with admin funcionality

---
+++ Transportsystem-GoogleMaps/AdminConsole/DatabaseController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using AutoMapper;
+using Transportsystem_GoogleMaps.Models;
+using Transportsystem_GoogleMaps.Dtos;
+
+namespace AdminConsole
+{
+    class DatabaseController
+    {
+        private ApplicationDbContext _context;
+
+        public DatabaseController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public void GetPackages()
+        {
+            var packages = _context.Packages
+                .ToList();
+                
+            for (int i = 1; i <= packages.Count(); i++)
+            {
+                Console.WriteLine(i + ".");
+                Console.WriteLine("    Content: " + packages.ElementAt(i - 1).Content);
+                Console.WriteLine("    Destination: " + packages.ElementAt(i - 1).Destination);
+                Console.WriteLine("------------------------------");
+            }
+            Console.ReadLine();
+        }
+
+        public void GetDrivers()
+        {
+            var drivers = _context.Drivers
+                .ToList();
+
+            for (int i = 1; i <= drivers.Count(); i++)
+            {
+                Console.WriteLine(i + ".");
+                Console.WriteLine("    Name: " + drivers.ElementAt(i - 1).Name);
+                Console.WriteLine("    Phone number: " + drivers.ElementAt(i -1).PhoneNumber);
+                Console.WriteLine("------------------------------");
+            }
+            Console.ReadLine();
+        }
+
+        public void SavePackage(Package p)
+        {
+            _context.Packages.Add(p);
+            _context.SaveChanges();
+        }
+
+        public void SaveDriver(Driver d)
+        {
+            _context.Drivers.Add(d);
+            _context.SaveChanges();
+        }
+
+
+    }
+}
---
+++ Transportsystem-GoogleMaps/AdminConsole/DisplayHandler.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transportsystem_GoogleMaps.Models;
+
+namespace AdminConsole
+{
+    class DisplayHandler
+    {
+        private DatabaseController _dbController = new DatabaseController();
+        private ConsoleColor _color = ConsoleColor.Magenta;
+
+        public void DiplayStart()
+        {
+            
+            bool stayInLoop = true;
+            while (stayInLoop)
+            {
+                Console.Clear();
+                Console.ForegroundColor = _color;
+                Console.WriteLine("MAIN MENU");
+                Console.WriteLine("-------------------------");
+                Console.WriteLine("1. Create new object");
+                Console.WriteLine("2. Display existing objects");
+                Console.WriteLine("Type \"e\" to exit");
+                Console.WriteLine("");
+                Console.ResetColor();
+                string selected = Console.ReadLine();
+                switch (selected)
+                {
+                    case "1":
+                        CreationMenu();
+                        break;
+                    case "2":
+                        ViewMenu();
+                        break;
+                    case "e":
+                        stayInLoop = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+                }
+            }
+        }
+
+        public void CreationMenu()
+        {
+            bool stayInLoop = true;
+            while (stayInLoop)
+            {
+                Console.Clear();
+                Console.ForegroundColor = _color;
+                Console.WriteLine("CREATION MENU");
+                Console.WriteLine("-------------------------");
+                Console.WriteLine("1. Package");
+                Console.WriteLine("2. Driver");
+                Console.WriteLine("Type \"e\" to return");
+                Console.ResetColor();
+                string selected = Console.ReadLine();
+                switch (selected)
+                {
+                    case "1":
+                        PackageCreation();
+                        break;
+                    case "2":
+                        DriverCreation();
+                        break;
+                    case "e":
+                        stayInLoop = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+
+                }
+            }
+            
+        }
+
+        public void ViewMenu()
+        {
+            bool stayInLoop = true;
+            while (stayInLoop)
+            {
+                Console.Clear();
+                Console.ForegroundColor = _color;
+                Console.WriteLine("VIEW MENU");
+                Console.WriteLine("-------------------------");
+                Console.WriteLine("1. Package");
+                Console.WriteLine("2. Driver");
+                Console.WriteLine("Type \"e\" to return");
+                Console.ResetColor();
+                string selected = Console.ReadLine();
+                switch (selected)
+                {
+                    case "1":
+                        PackageView();
+                        break;
+                    case "2":
+                        DriverView();
+                        break;
+                    case "e":
+                        stayInLoop = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+
+                }
+            }
+        }
+
+        public void DriverCreation()
+        {
+            Driver d = new Driver(); 
+            Console.Clear();
+            Console.ForegroundColor = _color;
+            Console.WriteLine("DRIVER CREATION");
+            Console.WriteLine("-------------------------");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.Write("Name: ");
+            d.Name = Console.ReadLine();
+            Console.Write("Phone: ");
+            d.PhoneNumber = Console.ReadLine();
+            _dbController.SaveDriver(d);
+        }
+
+        public void PackageCreation()
+        {
+            Package p  = new Package();
+            Console.Clear();
+            Console.ForegroundColor = _color;
+            Console.WriteLine("PACKAGE CREATION");
+            Console.WriteLine("-------------------------");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.Write("Content: ");
+            p.Content = Console.ReadLine();
+            Console.Write("Destination: ");
+            p.Destination = Console.ReadLine();
+            _dbController.SavePackage(p);
+
+        }
+
+        public void DriverView()
+        {
+            Console.Clear();
+            Console.WriteLine("DRIVER LIST");
+            Console.WriteLine("-------------------------");
+            _dbController.GetDrivers();
+        }
+
+        public void PackageView()
+        {
+            Console.Clear();
+            Console.WriteLine("PACKAGE LIST");
+            Console.WriteLine("-------------------------");
+            _dbController.GetPackages();
+        }
+    }
+}
